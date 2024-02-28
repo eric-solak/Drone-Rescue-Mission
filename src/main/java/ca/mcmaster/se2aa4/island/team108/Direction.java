2338d08d57220f2b635594a4309ecaed5c2283b7
eric-solak <solakeric28@gmail.com>
2024-02-28 14:47:21 -0500

    Island can be now be found with any map. Direction class updated. System now retrieves initial direction as well

--- src/main/java/ca/mcmaster/se2aa4/island/team108/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team108/Direction.java
@@ -2,38 +2,25 @@ package ca.mcmaster.se2aa4.island.team108;
 
 // Direction/Compass Enum
 public enum Direction {
-    NORTH, SOUTH, EAST, WEST;
+    N, S, E, W;
 
-    // implementing turnRight and turnLeft
+
     public Direction turnRight() {
-
-        switch (this) {
-            case NORTH:
-                return EAST;
-            case EAST:
-                return SOUTH;
-            case SOUTH:
-                return WEST;
-            case WEST:
-                return NORTH;
-            default:
-                return this; 
-        }
+        return switch (this) {
+            case N -> E;
+            case E -> S;
+            case S -> W;
+            case W -> N;
+        };
     }
 
     public Direction turnLeft() {
-
-        switch (this) {
-            case NORTH:
-                return WEST;
-            case WEST:
-                return SOUTH;
-            case SOUTH:
-                return EAST;
-            case EAST:
-                return NORTH;
-            default:
-                return this; 
-        }
+        return switch (this) {
+            case N -> W;
+            case W -> S;
+            case S -> E;
+            case E -> N;
+        };
     }
+
 }
--- src/main/java/ca/mcmaster/se2aa4/island/team108/FindIsland.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team108/FindIsland.java
@@ -1,52 +1,65 @@
 package ca.mcmaster.se2aa4.island.team108;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class FindIsland {
+    private final Logger logger = LogManager.getLogger();
 
-    public JSONObject noLandDetected(JSONObject prev) {
+    // Repeat "fly", "scan", and "echo" in both the left and right directions until land is found
+    public JSONObject noLandDetected(JSONObject prev, Direction heading) {
         JSONObject output = new JSONObject();
 
         try {
             if (prev.has("action")) {
                 String prevAction = prev.getString("action");
+                logger.info("This is the prev action: " + prevAction);
 
                 if (Objects.equals(prevAction, "fly")) {
                     output.put("action", "scan");
                 } else if (Objects.equals(prevAction, "scan")) {
                     output.put("action", "echo");
                     JSONObject parameters = new JSONObject();
-                    parameters.put("direction", "N");
+                    parameters.put("direction", heading.turnLeft());
                     output.put("parameters", parameters);
                 } else if (Objects.equals(prevAction, "echo")){
                     JSONObject parameters = prev.getJSONObject("parameters");
-                    String direction = parameters.getString("direction");
+                    Direction direction = (Direction) parameters.get("direction");
 
-                    if (Objects.equals(direction, "N")) {
+                    logger.info("Direction from parameters: " + direction);
+                    logger.info("Direction to the left: " + heading.turnLeft());
+
+                    if (direction == heading.turnLeft()) { // First echo iteration
+                        logger.info("Condition is true: direction equals heading.turnLeft()");
                         output.put("action", "echo");
-                        parameters.put("direction", "S");
+                        parameters.put("direction", heading.turnRight());
                         output.put("parameters", parameters);
-                    } else {
+                    } else { // Second echo iteration has been called, repeat the cycle and go back to "fly"
                         output.put("action", "fly");
                     }
                 }
             } else {
+                logger.info("Straight to else statement");
                 output.put("action", "fly"); // Default action
             }
         } catch (Exception e) {
+            logger.error("Exception: " + e.getMessage());
             output.put("action", "fly");
         }
 
         return output;
     }
 
+    // Land detected, fly in a straight line until the beach is reached
+    // This should be modified, as echo returns the distance to the land. It is unnecessary to
+    // call scan when the distance to the land is already known
     public JSONObject landDetected(JSONObject prev) {
         JSONObject output = new JSONObject();
         if (prev.has("action")) {
             String prevAction = prev.getString("action");
-
             if (Objects.equals(prevAction, "fly")) {
                 output.put("action", "scan");
             } else {
